dd6060c752d21138c374cc4188ffd009bf89bdb0
Zuzia <r0s3m4ry.r0s3@gmail.com>
2014-10-12 14:43:24 +0200

    ...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railgirls::Application.config.secret_key_base = '03b1bbe10f8f861755296d18d1d6eead59dcbb6df57375a46f73af5e8b543ac4343867ee247366a9d4253b4bf9c01e93065c5ff2fe3af29e6da5fbdc66455d21'
